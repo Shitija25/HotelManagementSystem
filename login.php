0b48a2dd1f7e4e86ca91113ac94ce66a4c168803
Shitija25 <117753048+Shitija25@users.noreply.github.com>
2024-06-19 15:19:54 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,14 +1,5 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: vishal
- * Date: 10/23/17
- * Time: 1:45 PM
- */ ?>
-<!--
-    you can substitue the span of reauth email for a input with the email and
-    include the remember me checkbox
-    -->
+
+
 <html>
 <head>
     <link href="css/bootstrap.min.css" rel="stylesheet">
